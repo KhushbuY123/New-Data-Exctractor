105293ebbe6ba859a28016d8b2b6286723cec7ce
Khushbu Yadav <khushbuyad123@gmail.com>
2024-08-30 12:02:29 +0530

    new

--- src/Components/file-converter.js
+++ src/Components/file-converter.js
@@ -1,5 +1,5 @@
 
-import { RemoveRedEye } from "@mui/icons-material"
+import { RemoveRedEye } from "@mui/icons-material";
 import {
   Box,
   Button,
@@ -7,131 +7,128 @@ import {
   Grid,
   IconButton,
   Stack,
-} from "@mui/material"
-import SplitPane from "react-split-pane"
-import Dialog from "@mui/material/Dialog"
-import DialogActions from "@mui/material/DialogActions"
-import DialogContent from "@mui/material/DialogContent"
-import DialogTitle from "@mui/material/DialogTitle"
-import React, { useEffect, useMemo, useState } from "react"
-import InvoiceEdit from "./InvoiceEditor"
-var pdfjsLib = window["pdfjs-dist/build/pdf"]
-pdfjsLib.GlobalWorkerOptions.workerSrc = "./assets/js/pdf.worker.js"
+} from "@mui/material";
+import { ResizableBox } from "react-resizable";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import React, { useEffect, useMemo, useState } from "react";
+import InvoiceEdit from "./InvoiceEditor";
+
+var pdfjsLib = window["pdfjs-dist/build/pdf"];
+pdfjsLib.GlobalWorkerOptions.workerSrc = "./assets/js/pdf.worker.js";
 
 function FileConverter({ pdfUrl }) {
-  const myRef = React.createRef()
+  const myRef = React.createRef();
 
-  const [open, setOpen] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [dataLoader, setDataLoader] = useState(false)
-  const [imageUrls, setImageUrls] = useState([])
-  const [numOfPages, setNumOfPages] = useState(0)
-  const [selectedImage, setSelectedImage] = useState(null)
-  const [extractedData, setExtractedData] = useState(null)
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [dataLoader, setDataLoader] = useState(false);
+  const [imageUrls, setImageUrls] = useState([]);
+  const [numOfPages, setNumOfPages] = useState(0);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [extractedData, setExtractedData] = useState(null);
 
   useEffect(() => {
-    setLoading(false)
-  }, [imageUrls])
+    setLoading(false);
+  }, [imageUrls]);
 
   const handleClickOpen = (url, index) => {
-    setSelectedImage({ url, index })
-    setOpen(true)
-  }
+    setSelectedImage({ url, index });
+    setOpen(true);
+  };
 
   const handleClose = () => {
-    setSelectedImage(null)
-    setOpen(false)
-  }
+    setSelectedImage(null);
+    setOpen(false);
+  };
 
   const UrlUploader = (url) => {
     fetch(url).then((response) => {
       response.blob().then((blob) => {
-        let reader = new FileReader()
+        let reader = new FileReader();
         reader.onload = (e) => {
-          const data = atob(e.target.result.replace(/.*base64,/, ""))
-          renderPage(data)
-        }
-        reader.readAsDataURL(blob)
-      })
-    })
-  }
+          const data = atob(e.target.result.replace(/.*base64,/, ""));
+          renderPage(data);
+        };
+        reader.readAsDataURL(blob);
+      });
+    });
+  };
 
   useMemo(() => {
-    UrlUploader(pdfUrl)
-  }, [])
+    UrlUploader(pdfUrl);
+  }, []);
 
   const renderPage = async (data) => {
-    setLoading(true)
-    const imagesList = []
-    const canvas = document.createElement("canvas")
-    canvas.setAttribute("className", "canv")
-    const pdf = await pdfjsLib.getDocument({ data }).promise
+    setLoading(true);
+    const imagesList = [];
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("className", "canv");
+    const pdf = await pdfjsLib.getDocument({ data }).promise;
     for (let i = 1; i <= pdf.numPages; i++) {
-      var page = await pdf.getPage(i)
-      var viewport = page.getViewport({ scale: 1.5 })
-      canvas.height = viewport.height
-      canvas.width = viewport.width
+      var page = await pdf.getPage(i);
+      var viewport = page.getViewport({ scale: 1.5 });
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
       var render_context = {
         canvasContext: canvas.getContext("2d"),
         viewport: viewport,
-      }
-      await page.render(render_context).promise
-      let img = canvas.toDataURL("image/png")
-      imagesList.push(img)
+      };
+      await page.render(render_context).promise;
+      let img = canvas.toDataURL("image/png");
+      imagesList.push(img);
     }
-    setNumOfPages((e) => e + pdf.numPages)
-    setImageUrls((e) => [...e, ...imagesList])
-  }
+    setNumOfPages((e) => e + pdf.numPages);
+    setImageUrls((e) => [...e, ...imagesList]);
+  };
 
   useEffect(() => {
-    myRef.current.scrollIntoView({ behavior: "smooth", block: "start" })
-  }, [imageUrls])
+    myRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  }, [imageUrls]);
 
   const ExtractData = async (base64Images) => {
-    setDataLoader(true)
+    setDataLoader(true);
     try {
       if (!base64Images || base64Images.length === 0) {
-        console.log("Please select at least one file")
-        return
+        console.log("Please select at least one file");
+        return;
       }
 
-   // Send the array of base64 images to the API
       const response = await fetch("http://localhost:4000/pdfupload", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ base64Images }), 
-      })
+        body: JSON.stringify({ base64Images }),
+      });
 
-      const data = await response.json()
-      setExtractedData(data?.extractedTexts)
+      const data = await response.json();
+      setExtractedData(data?.extractedTexts);
 
       if (!response.ok) {
-        throw new Error(data.message || "Something went wrong")
+        throw new Error(data.message || "Something went wrong");
       }
- 
-    // Set extracted text
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     } finally {
-      console.log("Finally")
-      setDataLoader(false)
+      console.log("Finally");
+      setDataLoader(false);
     }
-  }
-   // Clean the raw JSON string
+  };
+
   const cleanAndParseJson = (rawJson) => {
     const cleanedJsonString = rawJson
       .replace(/^```json\s*/, "")
-      .replace(/\s*```$/, "")
-   // Parse the cleaned JSON string
+      .replace(/\s*```$/, "");
     try {
-      const parsedJson = JSON.parse(cleanedJsonString)
-      return parsedJson
+      const parsedJson = JSON.parse(cleanedJsonString);
+      return parsedJson;
     } catch (error) {
-      console.error("Error parsing JSON:", error)
+      console.error("Error parsing JSON:", error);
     }
-  }
+  };
 
   return (
     <Box sx={{ my: 4, textAlign: "center" }} ref={myRef} id="image-container">
@@ -150,83 +147,75 @@ function FileConverter({ pdfUrl }) {
               >
                 Extract Data from PDF
               </Button>
-              <SplitPane
-                  split="vertical"
-                  defaultSize="50%"
-                  minSize={100}
-                  maxSize={-100}
-                  resizerStyle={{
-                    width: "5px",
-                    cursor: "col-resize",
-                    backgroundColor: "#ccc",
-                    margin: "0px 2px",
-                    height: "100%",
-                  }}
-                  paneStyle={{ overflow: "auto" }}
-                >
-                  <div>
-                    <Grid container spacing={3}>
-                      <Grid
-                        container
-                        item
-                        xs={12}
-                        sm={3}
-                        sx={{ marginLeft: { xs: "0", sm: "10rem" } }}
-                      >
-                        {imageUrls.map((url, index) => (
-                          <Grid item xs={12} key={index}>
-                            <Box
-                              sx={{
-                                width: "100%",
-                                height: "220px",
-                                cursor: "pointer",
-                              }}
-                              className="img-card"
-                            >
-                              <img
-                                src={url}
-                                alt={`Page ${index + 1}`}
-                                style={{
-                                  width: "100%",
-                                  height: "100%",
-                                  objectFit: "cover",
-                                }}
-                              />
-                              <Stack
-                                direction="row"
-                                spacing={1}
-                                sx={{ position: "absolute", top: 2, right: 2 }}
-                              >
-                                <IconButton
-                                  onClick={() => handleClickOpen(url, index)}
-                                  className="btn-bg"
-                                >
-                                  <RemoveRedEye />
-                                </IconButton>
-                              </Stack>
-                            </Box>
-                          </Grid>
-                        ))}
-                      </Grid>
-                    </Grid>
-                  </div>
-                  <div>
-                    {dataLoader ? (
-                      <Box sx={{ alignSelf: "center" }}>
-                        <CircularProgress />
-                      </Box>
-                    ) : (
-                      <Box>
-                        {extractedData?.map((item, index) => (
-                          <InvoiceEdit
-                            key={index}
-                            initialData={cleanAndParseJson(item)}
+              <Box display="flex">
+              <ResizableBox
+                width={500}
+                height={500}
+                minConstraints={[200, 400]}
+                maxConstraints={[900, 400]}
+                resizeHandles={["e"]}
+                axis="x"
+              >
+                <Grid container>
+                  <Grid
+                    container
+                    item
+                    xs={12}
+                    sm={3}
+                    sx={{ marginLeft: { xs: "0", sm: "10rem" } }}
+                  >
+                    {imageUrls.map((url, index) => (
+                      <Grid item xs={12} key={index}>
+                        <Box
+                          sx={{
+                            width: "100%",
+                            height: "220px",
+                            cursor: "pointer",
+                          }}
+                          className="img-card"
+                        >
+                          <img
+                            src={url}
+                            alt={`Page ${index + 1}`}
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              objectFit: "cover",
+                            }}
                           />
-                        ))}
-                      </Box>
-                    )}
-                  </div>
-                </SplitPane>
+                          <Stack
+                            direction="row"
+                            spacing={1}
+                            sx={{ position: "absolute", top: 2, right: 2 }}
+                          >
+                            <IconButton
+                              onClick={() => handleClickOpen(url, index)}
+                              className="btn-bg"
+                            >
+                              <RemoveRedEye />
+                            </IconButton>
+                          </Stack>
+                        </Box>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </Grid>
+              </ResizableBox>
+                {dataLoader ? (
+                  <Box sx={{ alignSelf: "center",margin:"auto"}}>
+                    <CircularProgress />
+                  </Box>
+                ) : (
+                  <Box>
+                    {extractedData?.map((item, index) => (
+                      <InvoiceEdit
+                        key={index}
+                        initialData={cleanAndParseJson(item)}
+                      />
+                    ))}
+                  </Box>
+                )}
+              </Box>
             </>
           )}
         </>
@@ -257,7 +246,7 @@ function FileConverter({ pdfUrl }) {
         </DialogActions>
       </Dialog>
     </Box>
-  )
+  );
 }
 
 export default FileConverter;
--- src/Components/img-file.js
+++ src/Components/img-file.js
@@ -1,29 +1,18 @@
+import React, { useState } from "react";
 import {
   Box,
   Button,
   CircularProgress,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
   Grid,
-  IconButton,
-  Stack,
 } from "@mui/material";
-import React, { useState } from "react";
+import { ResizableBox } from "react-resizable";
+import 'react-resizable/css/styles.css';
 import InvoiceEdit from "./InvoiceEditor";
-import { RemoveRedEye } from "@mui/icons-material";
-import SplitPane from "react-split-pane";
-
-var pdfjsLib = window["pdfjs-dist/build/pdf"];
-pdfjsLib.GlobalWorkerOptions.workerSrc = "./assets/js/pdf.worker.js";
 
 function ImgFile({ file }) {
-  const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [dataLoader, setDataLoader] = useState(false);
   const [jsonData, setJsonData] = useState();
-  const [selectedImage, setSelectedImage] = useState(null);
 
   const cleanAndParseJson = (rawJson) => {
     const cleanedJsonString = rawJson
@@ -35,16 +24,6 @@ function ImgFile({ file }) {
     } catch (error) {
       console.error("Error parsing JSON:", error);
     }
-  };
-
-  // const handleClickOpen = (url) => {
-  //   setSelectedImage({ url });
-  //   setOpen(true);
-  // };
-
-  const handleClose = () => {
-    setSelectedImage(null);
-    setOpen(false);
   };
 
   const ExtractData = async (file) => {
@@ -76,28 +55,19 @@ function ImgFile({ file }) {
       ) : (
         <>
           <h4 className="drop-file-preview__title">File</h4>
-          <Button sx={{ background: "lightblue",marginBottom:"20px" }} onClick={() => ExtractData(file)}>
+          <Button sx={{ background: "lightblue", marginBottom: "20px" }} onClick={() => ExtractData(file)}>
             Extract Data
           </Button>
-          <SplitPane
-            split="vertical"
-            defaultSize="50%"
-            minSize={100}
-            maxSize={-100}
-            resizerStyle={{
-              width: '5px',
-              cursor: 'col-resize',
-              backgroundColor: '#ccc',
-              margin: '0px 2px',
-              height: '100%',
-            }}
-            paneStyle={{ overflow: 'auto' }}
-          >
-            <Grid container item xs={12} sm={9} sx={{margin:"auto"}}>
-              <Box
-                sx={{ width: "100%", cursor: "pointer" }}
-                className="img-card"
-              >
+          <Box display="flex">
+            <ResizableBox
+              width={500}
+              height={500}
+              minConstraints={[200, 400]}
+              maxConstraints={[900, 400]}
+              axis="x"
+              resizeHandles={['e']}
+            >
+              <Box sx={{ width: "100%", height: "100%", cursor: "pointer" }}>
                 <img
                   src={URL.createObjectURL(file)}
                   alt="img"
@@ -107,66 +77,19 @@ function ImgFile({ file }) {
                     objectFit: "contain",
                   }}
                 />
-                {/* <Stack
-                  direction="row"
-                  spacing={1}
-                  sx={{ position: "absolute", top: 2, right: 2 }}
-                >
-                  <IconButton
-                    onClick={() => handleClickOpen(URL.createObjectURL(file))}
-                    className="btn-bg"
-                  >
-                    <RemoveRedEye />
-                  </IconButton>
-                </Stack> */}
               </Box>
-            </Grid>
-
-            <Grid
-              container
-              item
-              xs={9}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
+            </ResizableBox>
+            <Box flexGrow={1} sx={{ display: "flex", flexDirection: "column", overflow: "auto" }}>
               {jsonData && <InvoiceEdit initialData={jsonData} />}
               {dataLoader && (
                 <Box sx={{ alignSelf: "center" }}>
                   <CircularProgress />
                 </Box>
               )}
-            </Grid>
-          </SplitPane>
+            </Box>
+          </Box>
         </>
       )}
-
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        scroll={"paper"}
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-      >
-        <DialogTitle id="scroll-dialog-title">Preview</DialogTitle>
-        <DialogContent dividers={true}>
-          <img
-            src={selectedImage?.url}
-            alt={selectedImage?.url}
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-        </DialogContent>
-        <DialogActions sx={{ p: 2 }}>
-          <Button variant="outlined" onClick={handleClose}>
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
     </Box>
   );
 }
